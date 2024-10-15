2031895aa077fc09802374e4a1d4a84c8b3b813a
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-10-15 11:00:30 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Jimmy Corndog',
+    streetAddress: '123 Maple Dickway'
+};
+
+function updateEmployeeWithKeyAndValue(emp, key, val) {
+    const newEmployee = {...emp};
+    newEmployee[key] = val;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, val) {
+    emp[key] = val;
+    return emp;
+};
+
+function deleteFromEmployeeByKey(emp, key) {
+    const newEmployee = {...emp};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+    delete emp[key];
+    return emp;
+}
